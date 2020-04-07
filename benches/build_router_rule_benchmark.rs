af18415b409f5a9c1bcee3a8181ead2f2638af3b
Joel Wurtz <jwurtz@jolicode.com>
2020-04-07 13:26:43 +0200

    Add bench for building router and no match

---
+++ benches/build_router_rule_benchmark.rs
@@ -0,0 +1,51 @@
+#[macro_use]
+extern crate criterion;
+#[macro_use]
+extern crate log;
+#[path = "../src/router/mod.rs"]
+mod router;
+use criterion::{Criterion, BenchmarkId, BatchSize};
+use serde::{Serialize, Deserialize};
+use router::rule::Rule;
+use std::fs::read_to_string;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ApiAgentRuleResponse {
+    #[serde(rename = "hydra:member")]
+    pub rules: Vec<Rule>,
+}
+
+fn create_rules(filename: String) -> String {
+    let content = read_to_string(filename).expect("Cannot open file");
+    let api: ApiAgentRuleResponse = serde_json::from_str(content.as_str()).expect("Cannot deserialize");
+
+    serde_json::to_string(&api.rules).expect("Cannot serialize")
+}
+
+fn build_router_bench(c: &mut Criterion) {
+    let files = vec![
+        "../bench-files/large-rules-1k.json".to_string(),
+        "../bench-files/large-rules-10k.json".to_string(),
+        "../bench-files/large-rules-50k.json".to_string(),
+        "../bench-files/large-rules-150k.json".to_string(),
+        "../bench-files/large-rules-200k.json".to_string(),
+    ];
+
+    let mut group = c.benchmark_group("router_builder");
+
+    for filename in files {
+        group.sample_size(10);
+        group.bench_with_input(BenchmarkId::from_parameter(filename.clone()), &filename, |b, f| {
+            b.iter_batched(
+                || create_rules(f.to_string()),
+                |rules| router::MainRouter::new_from_data(rules, 0),
+                BatchSize::NumIterations(1),
+            );
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, build_router_bench);
+criterion_main!(benches);
--- benches/match_rule_benchmark.rs
+++ benches/match_rule_benchmark.rs
@@ -1,27 +1,78 @@
 #[macro_use]
 extern crate criterion;
+#[macro_use]
+extern crate log;
 #[path = "../src/router/mod.rs"]
 mod router;
-use criterion::black_box;
-use criterion::Criterion;
+use criterion::{Criterion, BenchmarkId, BatchSize};
+use serde::{Serialize, Deserialize};
+use router::rule::Rule;
+use std::fs::read_to_string;
 
-fn match_rule_bench_cache(c: &mut Criterion) {
-    let main_router = router::create_test_router(true);
-    c.bench_function("match_rule_bench_cache", move |b| {
-        b.iter(|| {
-            main_router.do_match("https://fr.ouibus.com/fr/montargis".to_string());
-        });
-    });
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ApiAgentRuleResponse {
+    #[serde(rename = "hydra:member")]
+    pub rules: Vec<Rule>,
 }
 
-fn match_rule_bench_nocache(c: &mut Criterion) {
-    let main_router = router::create_test_router(false);
-    c.bench_function("match_rule_bench_nocache", move |b| {
-        b.iter(|| {
-            main_router.do_match("https://fr.ouibus.com/fr/montargis".to_string());
-        });
-    });
+fn create_rules(filename: String) -> String {
+    let content = read_to_string(filename).expect("Cannot open file");
+    let api: ApiAgentRuleResponse = serde_json::from_str(content.as_str()).expect("Cannot deserialize");
+
+    serde_json::to_string(&api.rules).expect("Cannot serialize")
 }
 
-criterion_group!(benches, match_rule_bench_nocache, match_rule_bench_cache);
+fn no_match_bench(c: &mut Criterion) {
+    let files = vec![
+        "../bench-files/large-rules-1k.json".to_string(),
+        "../bench-files/large-rules-10k.json".to_string(),
+        "../bench-files/large-rules-50k.json".to_string(),
+        "../bench-files/large-rules-150k.json".to_string(),
+        "../bench-files/large-rules-200k.json".to_string(),
+    ];
+
+    let mut group = c.benchmark_group("no_match");
+    group.sample_size(10);
+
+    for filename in files {
+        let rules = create_rules(filename.clone());
+        let router = router::MainRouter::new_from_data(rules, 0).expect("Cannot create router");
+
+        group.bench_with_input(BenchmarkId::from_parameter(filename.clone()), &filename, |b, f| {
+            b.iter(|| {
+                router.match_rule("/no-match".to_string());
+            });
+        });
+    }
+
+    group.finish();
+}
+
+fn no_match_cache_bench(c: &mut Criterion) {
+    let files = vec![
+        "../bench-files/large-rules-1k.json".to_string(),
+        "../bench-files/large-rules-10k.json".to_string(),
+        "../bench-files/large-rules-50k.json".to_string(),
+        "../bench-files/large-rules-150k.json".to_string(),
+        "../bench-files/large-rules-200k.json".to_string(),
+    ];
+
+    let mut group = c.benchmark_group("no_match_cache");
+    group.sample_size(10);
+
+    for filename in files {
+        let rules = create_rules(filename.clone());
+        let router = router::MainRouter::new_from_data(rules, 1000).expect("Cannot create router");
+
+        group.bench_with_input(BenchmarkId::from_parameter(filename.clone()), &filename, |b, f| {
+            b.iter(|| {
+                router.match_rule("/no-match".to_string());
+            });
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, no_match_bench, no_match_cache_bench);
 criterion_main!(benches);
