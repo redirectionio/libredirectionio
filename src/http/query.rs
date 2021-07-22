a6be191a663abae8c7d811d490f4650c45f97ce3
Joel Wurtz <joel.wurtz@gmail.com>
2021-07-22 12:58:49 +0200

    Better error message

--- src/http/query.rs
+++ src/http/query.rs
@@ -48,7 +48,11 @@ impl PathAndQueryWithSkipped {
         let path_and_query: PathAndQuery = match url.parse() {
             Ok(p) => p,
             Err(err) => {
-                log::error!("cannot parse url {}, don't ignore markerting query params: {}", url, err);
+                log::error!(
+                    "cannot parse url '{}', cancel ignoring marketing query params: {}",
+                    path_and_query_str,
+                    err
+                );
 
                 return Self {
                     path_and_query_matching: Some(if config.ignore_path_and_query_case {
